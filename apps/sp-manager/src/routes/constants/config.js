ab7bf263c4277fbe4a93139d6f87af59ab85af81
Navneet Golwalkar <ngolwalkar@outlook.com>
2022-09-10 12:47:19 +0530

    Added Separate Okta App

--- apps/sp-manager/src/routes/constants/config.js
+++ apps/sp-manager/src/routes/constants/config.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = '0oa615lg7bWiaMbB25d7';
+const CLIENT_ID = '0oa6ggwsbrNows2JN5d7';
 const ISSUER = 'https://dev-10972897.okta.com/oauth2/default';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 const REDIRECT_URI = `${window.location.origin}/login/callback`;
